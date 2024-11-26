7ddc2654e1db263a7edee66bad288324547c160c
unknown <wenzhe332@gmail.com>
2024-11-26 16:52:43 +0800

    The first code

---
+++ Person.java
@@ -0,0 +1,10 @@
+public class Person {
+
+    public static void main(String[] args) {
+        String name = "GOH";
+        int age = 22;
+        double height = 1.77;
+        System.out.println("The student name is: " + name + " and age is: " + age);
+        System.out.println("The student height is: " + height);
+    }
+}
